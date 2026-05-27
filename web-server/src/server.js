18c6e0f35c614297d8a438330b0551a93339083d
sidk524 <siddharthkambli6@gmail.com>
2026-05-27 12:34:54 +0100

    cd test

--- web-server/src/server.js
+++ web-server/src/server.js
@@ -19,10 +19,7 @@ app.get("/", (_req, res) => {
 });
 
 app.get("/health", (_req, res) => {
-  res.json({
-    status: "healthy",
-    uptime: process.uptime()
-  });
+  res.type("text").send("VERY HEALTHY. VERY AWESOME");
 });
 
 app.use((_req, res) => {
