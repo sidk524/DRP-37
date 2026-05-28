93dd1e728c9ddc05052817bc14f9ac4926cbcc73
Bartlomiej Dlugosz <bartlomiejd15@gmail.com>
2026-05-28 17:49:08 +0100

    Test: Breaking change

--- web-server/src/server.js
+++ web-server/src/server.js
@@ -12,10 +12,7 @@ app.use(helmet());
 app.use(express.json());
 
 app.get("/", (_req, res) => {
-  res.json({
-    name: "DRP-37 web server",
-    status: "ok"
-  });
+
 });
 
 app.get("/health", (_req, res) => {
